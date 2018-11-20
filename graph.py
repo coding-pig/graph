261e830c35c115921e86b9187580f772876f29af
Zhiming Qiu <zhiming.qiu@move.com>
2018-11-19 21:06:54 -0800

    Refactor to prepare for dedup

--- graph.py
+++ graph.py
@@ -11,14 +11,13 @@ class GraphNode:
     # return list of all reachable nodes from self via bfs
     # nearer nodes appear earlier in returned list
     def bfs(self):
-        toOpen, toReturn = [self], [] 
+        toOpen, toReturn = list(self._to), [] 
         while len(toOpen) > 0:
             top = toOpen.pop(0)
+            toReturn.append(top)
             for to in top._to:
                 if to not in toReturn: # O(N) check. Use a set to make it O(1)
-                    toReturn.append(to)
-                    if to is not self:
-                        toOpen.append(to)
+                    toOpen.append(to)
 
         return toReturn 
 
