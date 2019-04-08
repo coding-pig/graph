abf5eb791a39b87840e54110ee7a3fd7dd722c56
lol <2784472+qdxlqy2001@users.noreply.github.com>
2019-04-08 12:37:25 -0700

    Set up function def (#2)

---
+++ test/test_toposort.py
@@ -0,0 +1,17 @@
+from src import toposort
+import unittest
+
+class TestTopoSort(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_toposort(self):
+        self.assertEqual(type(toposort.toposort), type(lambda:0))
+        self.assertRaises(Exception, toposort.toposort)
+
+
+if __name__ == "__main__":
+    unittest.main()
