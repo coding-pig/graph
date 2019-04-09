cdf44fcf78bc3b91267bf977e71618abc60507d3
Zhiming Qiu <zhiming.qiu@move.com>
2019-04-08 17:28:00 -0700

    Init implementation

--- src/toposort.py
+++ src/toposort.py
@@ -2,5 +2,16 @@ from src import graph
 
 
 def toposort(nodes):
-    # TODO
-    raise Exception("NOT IMPLEMENTED!")
+    output = []
+    done = set()
+    while len(done) != len(nodes):
+        done_new = []
+        for node in nodes:
+            if node in done:
+                continue
+            if not [v for v in node._to if v not in done]:
+                done_new.append(node)
+        output.append(done_new)
+        for n in done_new:
+            done.add(n)
+    return output
--- test/test_toposort.py
+++ test/test_toposort.py
@@ -1,5 +1,9 @@
 from src import toposort
+from src import graph
 import unittest
+
+
+GraphNode = graph.GraphNode
 
 
 class TestTopoSort(unittest.TestCase):
@@ -9,9 +13,32 @@ class TestTopoSort(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_toposort(self):
+    def test_toposort_1(self):
+        a = GraphNode('a')
+        b = GraphNode('b')
+        b.to(a)
+        c = GraphNode('c')
+        c.to(a)
+        d = GraphNode('e')
+        d.to(c)
+        e = GraphNode('e')
+        e.to(a)
+        e.to(b)
+        e.to(d)
+
         self.assertEqual(type(toposort.toposort), type(lambda: 0))
-        self.assertRaises(Exception, toposort.toposort)
+        self.assertEqual(toposort.toposort([a, b, c, d, e]),
+                         [[a], [b, c], [d], [e]])
+
+    def test_toposort_2(self):
+        a = GraphNode('a')
+        b = GraphNode('b')
+        b.to(a)
+
+        self.assertSequenceEqual(toposort.toposort([a, b]), [[a], [b]])
+
+    def test_toposort_cycle(self):
+        pass
 
 
 if __name__ == "__main__":
